8c8b374cb5f9446dda22ef526dd429f644902e1d
euronion <42553970+euronion@users.noreply.github.com>
2023-08-16 17:02:41 +0200

    Update for PyPSA-EUR post-0.8.1 (current master)

--- scripts/iterative/RunPyPSA-Eur.sh
+++ scripts/iterative/RunPyPSA-Eur.sh
@@ -13,9 +13,9 @@ cp REMIND2PyPSAEUR.gdx ${1}/resources/${scenario}/i${2}/REMIND2PyPSAEUR.gdx
 # Source micromamba environment
 source /home/jhampp/software/micromamba_1.4.2/etc/profile.d/micromamba.sh
 # Start PyPSA-Eur
-module load gams/42.1.0
-micromamba run --name pypsa-eur-python-3-10 snakemake --profile ${1}/cluster_config/ -s ${1}/Snakefile_remind --directory ${1} results/${scenario}/i${2}/PyPSAEUR2REMIND.gdx
-module unload gams/42.1.0
+module load gams/43.4.1
+micromamba run --name pypsa-eur snakemake --profile ${1}/cluster_config/ -s ${1}/Snakefile_remind --directory ${1} results/${scenario}/i${2}/PyPSAEUR2REMIND.gdx
+module unload gams/43.4.1
 # Copy PyPSAEUR2REMIND.gdx to REMIND scenario directory
 cp ${1}/results/${scenario}/i${2}/PyPSAEUR2REMIND.gdx PyPSAEUR2REMIND.gdx
 cp ${1}/results/${scenario}/i${2}/PyPSAEUR2REMIND.gdx PyPSAEUR2REMIND_${2}.gdx
